72fc71a21c1f700285a839b7264b4e77b85f11cb
kalilcazes <kalilcazes@gmail.com>
2020-10-08 22:39:45 -0300

    refactor: extract code to functions

--- proxy/main.go
+++ proxy/main.go
@@ -4,10 +4,20 @@ import (
 	"crypto/tls"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
+
+const port = ":8000"
+const proto = "tcp"
+
+//ReverseProxy struct contains *http.Server and net.Listener fields
+type ReverseProxy struct {
+	server   *http.Server
+	listener net.Listener
+}
 
 func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -19,45 +29,67 @@ func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) 
 
 func redirect(from string, to string) {
 
-	remote, err := url.Parse(to)
+	redirectedURL, err := url.Parse(to)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-	http.HandleFunc(from, handler(proxy))
+	reverseProxy := httputil.NewSingleHostReverseProxy(redirectedURL)
+	http.HandleFunc(from, handler(reverseProxy))
 
 }
 
-func main() {
+func createCertificate(cert string, key string) tls.Certificate {
+
+	appCert, err := tls.LoadX509KeyPair(cert, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return appCert
+
+}
+
+func (rp *ReverseProxy) setUpTLS() {
 	var err error
+	var cert = []string{"localhost1.crt", "localhost2.crt"}
+	var key = []string{"localhost1.key", "localhost2.key"}
 
-	appCert1, err := tls.LoadX509KeyPair("localhost1.crt", "localhost1.key")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	appCert2, err := tls.LoadX509KeyPair("localhost2.crt", "localhost2.key")
-	if err != nil {
-		log.Fatal(err)
-	}
+	appCert1 := createCertificate(cert[0], key[0])
+	appCert2 := createCertificate(cert[1], key[1])
 
 	tlsConfig := &tls.Config{Certificates: []tls.Certificate{appCert1, appCert2}}
 
 	tlsConfig.BuildNameToCertificate()
 
-	proxy := &http.Server{
+	rp.server = &http.Server{
 		TLSConfig: tlsConfig,
 	}
+
+	rp.listener, err = tls.Listen(proto, port, tlsConfig)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+//NewReverseProxy create reverse proxy using TLS
+func NewReverseProxy() *ReverseProxy {
+
+	reverseProxy := &ReverseProxy{}
 
 	redirect("localhost1/", "http://localhost:8080")
 	redirect("localhost2/", "http://localhost:8081")
 
-	listener, err := tls.Listen("tcp", ":8000", tlsConfig)
+	reverseProxy.setUpTLS()
 
+	return reverseProxy
+}
+
+func main() {
+
+	reverseProxy := NewReverseProxy()
 	fmt.Println("Proxy running on port 8000...")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Fatal(proxy.Serve(listener))
+	log.Fatal(reverseProxy.server.Serve(reverseProxy.listener))
 }
