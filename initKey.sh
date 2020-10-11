867c2afe62cc31c91ab74cc7ed08d0f97497bd3a
kalilcazes <kalilcazes@gmail.com>
2020-10-11 10:40:43 -0300

    feat: remove the need to run as sudo

--- initKey.sh
+++ initKey.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-grep "127.0.0.1 localhost localhost1 localhost2" /etc/hosts &> /dev/null
-
-if [[ $? -ne 0 ]]; then
-    echo -e "\n127.0.0.1 localhost localhost1 localhost2" >> /etc/hosts
-fi
+export HOSTALIASES=~/.hosts
 
 echo "$(tput setaf 2)----------------------------------------------------------------------$(tput sgr 0)"
 
+grep "127.0.0.1 localhost localhost1 localhost2" /etc/hosts &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+   echo -e "\n127.0.0.1 localhost localhost1 localhost2" >> ~/.hosts
+fi
+
 echo "$(tput setaf 2)[INFO]$(tput sgr 0) Removing previous generated keys"
 rm -f proxy/*.key && rm -f proxy/*.crt && rm -f proxy/*.csr && rm -f proxy/*.log 2> /dev/null
-
 
 echo "$(tput setaf 2)[INFO]$(tput sgr 0) Creating self-signed certificate for localhost1 on dir proxy..."
 openssl req  -new  -newkey rsa:2048  -nodes  -keyout proxy/localhost1.key -subj "/CN=localhost1/emailAddress=admin@mail/C=BR/ST=RJ/L=Rio de Janeiro/O=Empty/OU=Empty"  -out proxy/localhost1.csr &> proxy/certificate1.log
@@ -21,7 +22,5 @@ openssl req  -new  -newkey rsa:2048  -nodes  -keyout proxy/localhost2.key -subj 
 openssl  x509  -req  -days 365  -in proxy/localhost2.csr  -signkey proxy/localhost2.key  -out proxy/localhost2.crt &>> proxy/certificate2.log
 
 echo "$(tput setaf 2)[INFO]$(tput sgr 0) Creating log file for operation above on dir proxy..."
+
 echo "$(tput setaf 2)----------------------------------------------------------------------$(tput sgr 0)"
-
-
-docker-compose up
